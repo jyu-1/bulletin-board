97c5a5f6eb26d1917f86f4440474f93e29b89617
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-20 09:55:43 -0800

    add time format

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,21 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/message.scss";
 
 interface ApiType {
     _id: React.Key;
     name: String;
-    createdAt: String;
+    createdAt: string;
     message: String;
 }
 
 const Messages = () => {
     const [message, setMessage] = useState<ApiType[]>([]);
+    const lastMessage = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         fetch(process.env.REACT_APP_API_HOST!)
             .then((res) => res.json())
             .then((result) => setMessage(result));
     }, []);
+
+    useEffect(() => {
+        if (lastMessage.current) {
+            lastMessage.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [message]);
 
     return (
         <div className="messages">
@@ -24,9 +31,14 @@ const Messages = () => {
                     <div key={item._id} className="message">
                         <div>
                             <span>{item.name}</span>
-                            <span>{item.createdAt}</span>
+                            <span>
+                                {new Date(Date.parse(item.createdAt))
+                                    .toString()
+                                    .slice(4, 21)}
+                            </span>
                         </div>
                         <div>{item.message}</div>
+                        <div ref={lastMessage} />
                     </div>
                 );
             })}
