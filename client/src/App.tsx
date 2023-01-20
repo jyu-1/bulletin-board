6b8346fc6cce01e4ff028cac2a18d6d5f5c4a846
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-20 10:10:06 -0800

    add mobile support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 function App() {
     return (
         <div className="App">
+            <header>Bulletin</header>
             <Main />
             <Footer />
         </div>
