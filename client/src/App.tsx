baffcb05358e431c0e8474302be6c98a4eb742db
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-20 22:48:50 -0800

    add button to play background music

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,40 @@
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import rainSound from "./assets/rainy.mp3";
+import playButton from "./assets/play.png";
+import { useRef, useState } from "react";
 
 function App() {
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const [audioPlay, setAudioPlay] = useState(false);
+
+    const playAudio = () => {
+        setAudioPlay(true);
+        audioRef.current?.play();
+    };
+
+    const pauseAudio = () => {
+        setAudioPlay(false);
+        audioRef.current?.pause();
+    };
+
     return (
         <div className="App">
-            <header>Bulletin</header>
+            <header>
+                Bulletin
+                <img
+                    className={audioPlay ? "active" : "inactive"}
+                    src={playButton}
+                    alt=""
+                    onClick={() =>
+                        audioRef.current?.paused ? playAudio() : pauseAudio()
+                    }
+                />
+            </header>
             <Main />
+            <audio ref={audioRef} loop>
+                <source src={rainSound} />
+            </audio>
             <Footer />
         </div>
     );
